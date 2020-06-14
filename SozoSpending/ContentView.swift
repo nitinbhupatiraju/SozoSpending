bc7886f32a81405147c4a1b69290ec298c84073e
Nitin Bhupatiraju <nitin.bhupatiraju@gmail.com>
2020-06-13 21:59:22 -0700

    initial commit

---
+++ SozoSpending/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SozoSpending
+//
+//  Created by Nitin Bhupatiraju on 6/13/20.
+//  Copyright © 2020 Nitin Bhupatiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
